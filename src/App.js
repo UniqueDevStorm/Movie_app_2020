dc2dd2f7700d3a828b7387ece1bf1631cced31ad
unknown <storm@uniquecode.team>
2021-02-06 15:21:10 +0900

    feat: Navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Home from "./routers/Home";
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from "./routers/About";
+import Navigation from "./components/Navigation";
 
 function App() {
     return (
         <HashRouter>
+            <Navigation />
             <Route path='/' exact component={Home} />
             <Route path='/about' component={About} />
         </HashRouter>
