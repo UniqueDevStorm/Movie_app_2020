69ad48b9810c12ab4f42fdaeb037a591d2f680bf
unknown <storm@uniquecode.team>
2021-02-06 16:04:53 +0900

    fix: warning

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
-import { Link } from "react-router-dom";
 
 function Movie({ title, year, summary, poster, genres }) {
     return (
